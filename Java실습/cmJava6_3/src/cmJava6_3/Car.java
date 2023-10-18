d939a0f0fed5b8f8e66354be1e1e1abdf1ef2dca
JustOneSeason <129948655+JustOneSeason@users.noreply.github.com>
2023-10-18 21:59:54 +0900

    6_3

--- Java실습/cmJava6_3/src/cmJava6_3/Car.java
+++ Java실습/cmJava6_3/src/cmJava6_3/Car.java
@@ -2,8 +2,53 @@ package cmJava6_3;
 
 public class Car {
 	
+	String company ="현대 자동차";
+	String model;
+	String color;
+	int  maxSpeed;
 	
-	Car(String color, int cc){
+	//this()로
+	Car(){
 		
 	}
+	
+	Car(String model){
+		this(model,"은색", 250);
+	}
+	
+	Car(String model,String color){
+		this(model,"color", 233);
+	}
+	
+	Car(String model, String color, int maxSpeed){
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	
+	
+// 오버로딩	
+//	Car(){
+//		
+//	}
+//	
+//	Car(String model){
+//		this.model = model;
+//	}
+//	
+//	Car(String model, String color){
+//		this.model = model;
+//		this.color = color;
+//	}
+//	
+//	Car(String model, String color, int maxSpeed){
+//		this.model = model;
+//		this.color = color;
+//		this.maxSpeed = maxSpeed;
+//	}오버로딩 생성자를 여러개 선언 생성자의 클래스와 타입 매개변수를 보고 판단 주의사항 매개변수의 순서는 똑같아야한다
+	 //this 객체 자신의 참조
+//	Car(String color, int cc){
+//		
+//	}
 }
